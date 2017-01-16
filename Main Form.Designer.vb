2f5b6c2433211b09298fc05077f8885bea661beb
sjahaf <salj4215@google.com>
2017-01-16 00:13:08 -0500

    All Code

---
+++ Main Form.Designer.vb
@@ -0,0 +1,241 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtDonutsOrdered = New System.Windows.Forms.TextBox()
+        Me.txtBagelsOrdered = New System.Windows.Forms.TextBox()
+        Me.txtCoffeeOrdered = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblSubtotal = New System.Windows.Forms.Label()
+        Me.lblSalesTax = New System.Windows.Forms.Label()
+        Me.lblTotalSales = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnClear = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(4, 132)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(200, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Enter the number of donuts ordered:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(7, 168)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(197, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Enter the number of bagels ordered:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(45, 204)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(159, 15)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Enter cups of coffee ordered:"
+        '
+        'txtDonutsOrdered
+        '
+        Me.txtDonutsOrdered.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtDonutsOrdered.Location = New System.Drawing.Point(210, 129)
+        Me.txtDonutsOrdered.Name = "txtDonutsOrdered"
+        Me.txtDonutsOrdered.Size = New System.Drawing.Size(100, 23)
+        Me.txtDonutsOrdered.TabIndex = 0
+        '
+        'txtBagelsOrdered
+        '
+        Me.txtBagelsOrdered.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtBagelsOrdered.Location = New System.Drawing.Point(210, 165)
+        Me.txtBagelsOrdered.Name = "txtBagelsOrdered"
+        Me.txtBagelsOrdered.Size = New System.Drawing.Size(100, 23)
+        Me.txtBagelsOrdered.TabIndex = 1
+        '
+        'txtCoffeeOrdered
+        '
+        Me.txtCoffeeOrdered.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtCoffeeOrdered.Location = New System.Drawing.Point(210, 201)
+        Me.txtCoffeeOrdered.Name = "txtCoffeeOrdered"
+        Me.txtCoffeeOrdered.Size = New System.Drawing.Size(100, 23)
+        Me.txtCoffeeOrdered.TabIndex = 2
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(150, 281)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(54, 15)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "Subtotal:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(150, 317)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(54, 15)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "Sales tax:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(145, 353)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(59, 15)
+        Me.Label6.TabIndex = 8
+        Me.Label6.Text = "Total sale:"
+        '
+        'lblSubtotal
+        '
+        Me.lblSubtotal.BackColor = System.Drawing.SystemColors.Control
+        Me.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSubtotal.Location = New System.Drawing.Point(210, 273)
+        Me.lblSubtotal.Name = "lblSubtotal"
+        Me.lblSubtotal.Size = New System.Drawing.Size(100, 23)
+        Me.lblSubtotal.TabIndex = 4
+        Me.lblSubtotal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblSalesTax
+        '
+        Me.lblSalesTax.BackColor = System.Drawing.SystemColors.Control
+        Me.lblSalesTax.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblSalesTax.Location = New System.Drawing.Point(210, 309)
+        Me.lblSalesTax.Name = "lblSalesTax"
+        Me.lblSalesTax.Size = New System.Drawing.Size(100, 23)
+        Me.lblSalesTax.TabIndex = 10
+        Me.lblSalesTax.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'lblTotalSales
+        '
+        Me.lblTotalSales.BackColor = System.Drawing.SystemColors.Control
+        Me.lblTotalSales.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblTotalSales.Location = New System.Drawing.Point(210, 345)
+        Me.lblTotalSales.Name = "lblTotalSales"
+        Me.lblTotalSales.Size = New System.Drawing.Size(100, 23)
+        Me.lblTotalSales.TabIndex = 11
+        Me.lblTotalSales.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(374, 239)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 3
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(160, 16)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(271, 40)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Crispies Bagels and Bites"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Assignment_1.My.Resources.Resources.johnny_automatic_bagel_300px
+        Me.PictureBox1.Location = New System.Drawing.Point(30, 6)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(112, 87)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 14
+        Me.PictureBox1.TabStop = False
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(374, 361)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 4
+        Me.btnClear.Text = "Clea&r"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(461, 396)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblTotalSales)
+        Me.Controls.Add(Me.lblSalesTax)
+        Me.Controls.Add(Me.lblSubtotal)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtCoffeeOrdered)
+        Me.Controls.Add(Me.txtBagelsOrdered)
+        Me.Controls.Add(Me.txtDonutsOrdered)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "frmMain"
+        Me.Text = "Main Form"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtDonutsOrdered As System.Windows.Forms.TextBox
+    Friend WithEvents txtBagelsOrdered As System.Windows.Forms.TextBox
+    Friend WithEvents txtCoffeeOrdered As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents lblSubtotal As System.Windows.Forms.Label
+    Friend WithEvents lblSalesTax As System.Windows.Forms.Label
+    Friend WithEvents lblTotalSales As System.Windows.Forms.Label
+    Friend WithEvents btnCalculate As System.Windows.Forms.Button
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents btnClear As System.Windows.Forms.Button
+
+End Class
---
+++ Main Form.vb
@@ -0,0 +1,83 @@
+﻿'Written by Salleh Jahaf
+'POS Program written to calculate subtotal, sales tax, and total sale when clerk enters quantity of donuts, bagels, and/or coffee customer buys
+'Program v1 completed on 09/14/2015
+'Updated on 09/15/2015, program v1.01
+
+Public Class frmMain
+    'Declare strClerk variable for clerk's name
+    Dim strClerk As String
+
+    'Load prompt for clerk's name
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Const strPrompt As String = "Enter your name:"
+        Const strTitle As String = "Name Entry"
+
+        strClerk = InputBox(strPrompt, strTitle)
+
+        'Output clerk's name to title bar of main form 
+        Me.Text = "Order Created By " & strClerk
+
+
+    End Sub
+
+    'Clear all outputs when text within textboxes are changed
+    Private Sub ClearAllOutput(sender As Object, e As EventArgs) Handles txtDonutsOrdered.TextChanged, txtBagelsOrdered.TextChanged, txtCoffeeOrdered.TextChanged
+        lblSubtotal.Text = String.Empty
+        lblSalesTax.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+
+    End Sub
+
+    'process prices and totals
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Const decDONUT_PRICE As Decimal = 1.09D
+        Const decBAGEL_PRICE As Decimal = 0.99D
+        Const decCOFFEE_PRICE As Decimal = 1.2D
+        Const decTAX_RATE As Decimal = 0.06D 'Tax rate for Michigan
+        Dim decPriceOfDonuts As Decimal
+        Dim decPriceOfBagels As Decimal
+        Dim decPriceOfCoffee As Decimal
+        Dim intNumDonuts As Integer
+        Dim intNumBagels As Integer
+        Dim intNumCoffee As Integer
+        Dim decSubtotal As Decimal
+        Dim decSalesTax As Decimal
+        Dim decTotalSales As Decimal
+
+        'Convert inputs to numbers
+        Integer.TryParse(txtDonutsOrdered.Text, intNumDonuts)
+        Integer.TryParse(txtBagelsOrdered.Text, intNumBagels)
+        Integer.TryParse(txtCoffeeOrdered.Text, intNumCoffee)
+
+        'process prices
+        decPriceOfDonuts = intNumDonuts * decDONUT_PRICE
+        decPriceOfBagels = intNumBagels * decBAGEL_PRICE
+        decPriceOfCoffee = intNumCoffee * decCOFFEE_PRICE
+        decSubtotal = decPriceOfDonuts + decPriceOfBagels + decPriceOfCoffee
+        decSalesTax = decSubtotal * decTAX_RATE
+        decTotalSales = decSubtotal + decSalesTax
+
+        'output
+        lblSubtotal.Text = decSubtotal.ToString("C2")
+        lblSalesTax.Text = decSalesTax.ToString("C2")
+        lblTotalSales.Text = decTotalSales.ToString("C2")
+
+
+    End Sub
+
+    'Clear all inputs and outputs when clicking Clear button
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+
+        'Clear all fields
+        txtDonutsOrdered.Text = String.Empty
+        txtBagelsOrdered.Text = String.Empty
+        txtCoffeeOrdered.Text = String.Empty
+        lblSubtotal.Text = String.Empty
+        lblSalesTax.Text = String.Empty
+        lblTotalSales.Text = String.Empty
+
+        Call txtDonutsOrdered.Focus()
+
+
+    End Sub
+End Class
